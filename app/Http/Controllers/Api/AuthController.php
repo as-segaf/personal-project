6904adac8d394d6519b157e01545257e8cd7410a
as-segaf <ziyanassegaf20@gmail.com>
2020-11-24 07:11:22 +0700

    handle register request

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,41 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $loginRequest)
+    {
+        if (Auth::attempt(['email' => $loginRequest->email, 'password' => $loginRequest->password])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('PodcastApp')->accessToken;
+            $success['name'] = $user->name;
+
+            return response()->json(['success' => $success], 200);
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function register(RegisterRequest $registerRequest)
+    {
+        $user = User::create([
+            'name' => $registerRequest->name,
+            'email' => $registerRequest->email,
+            'username' => $registerRequest->username,
+            'password' => Hash::make($registerRequest->password)
+        ]);
+
+        if ($user) {
+            $success['token'] = $user->createToken('PodcastApp')->accessToken;
+            $success['name'] = $user->name;
+
+            return response()->json(['success' => $success], 201);
+        }
+        return response()->json(['error' => 'Failed to register a user'], 500);
+    }
 }
