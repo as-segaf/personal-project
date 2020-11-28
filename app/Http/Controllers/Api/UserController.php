1bd39e37a7f0c20ae96a9b58e431e9f9e27cb43e
fesasabil <kijee212@gmail.com>
2020-11-28 08:42:09 +0700

    add passport in login and register

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Registration
+     */
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'username' => 'required|min:5',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+ 
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+       
+        $token = $user->createToken('LaravelAuthApp')->accessToken;
+        $success = $user->name; 
+ 
+        return response()->json(['success' => $success,'token' => $token], 200);
+    }
+ 
+    /**
+     * Login
+     */
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+ 
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }   
+}
