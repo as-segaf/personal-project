3867ea13d9ce200c8801fc36f9b2617e1e24bf63
as-segaf <ziyanassegaf20@gmail.com>
2020-12-17 22:30:07 +0700

    add routes for genre

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GenreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function() {
+    Route::get('genres', [GenreController::class, 'index']);
+    Route::get('genre/{genre}', [GenreController::class, 'show']);
 });
